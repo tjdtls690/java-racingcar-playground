826f299a7fc69dc3555c2277d64ddc6045a87ea6
JunH <tjdtls690@gmail.com>
2022-06-30 20:54:43 +0900

    :sparkles: feat : 쉼표 구분자를 통해 분리 후 덧셈 진행 기능 구현

--- src/main/java/com/jun_hyeok/string_calculator/domain/StringCalculator.java
+++ src/main/java/com/jun_hyeok/string_calculator/domain/StringCalculator.java
@@ -1,12 +1,42 @@
 package com.jun_hyeok.string_calculator.domain;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StringCalculator {
 
     public int splitAndSum(String input) {
-        if(input == null || input.isEmpty()){
+        if (input == null || input.isEmpty()) {
             return 0;
         }
+        input = input.replace(" ", "");
+        if (input.length() == 1) {
+            return Integer.parseInt(input);
+        }
 
-        return Integer.parseInt(input);
+        return getSum(input);
     }
+
+    private int getSum(String input) {
+        String[] split = input.split(",");
+        List<String> strings = Arrays.asList(split);
+        List<Integer> integerList = stringListToIntegerList(strings);
+        return add(integerList);
+    }
+
+    private int add(List<Integer> integerList) {
+        int sum = 0;
+        for(int num : integerList){
+            sum += num;
+        }
+        return sum;
+    }
+
+    private List<Integer> stringListToIntegerList(List<String> strings) {
+        return strings.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
 }
--- src/test/java/com/jun_hyeok/string_calculator/domain/StringCalculatorTest.java
+++ src/test/java/com/jun_hyeok/string_calculator/domain/StringCalculatorTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTest {
     StringCalculator s;
@@ -19,7 +20,7 @@ public class StringCalculatorTest {
 
     @Test
     @DisplayName("숫자 하나")
-    void num1(){
+    void splitAndSum_num1(){
         int result = s.splitAndSum("2");
         assertThat(result).isEqualTo(2);
     }
@@ -27,8 +28,16 @@ public class StringCalculatorTest {
     @ParameterizedTest
     @DisplayName("빈 문자열 또는 null")
     @NullAndEmptySource
-    void null_or_empty(String inputStr){
+    void splitAndSum_null_or_empty(String inputStr){
         int result = s.splitAndSum(inputStr);
         assertThat(result).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @DisplayName("쉼표 구분자")
+    @CsvSource(value = {"2, 5, 10; 17", "100, 5, 10; 115"}, delimiter = ';')
+    void splitAndSum_restSeparator(String inputStr, int assertNum){
+        int result = s.splitAndSum(inputStr);
+        assertEquals(assertNum, result);
     }
 }
