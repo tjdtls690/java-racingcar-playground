c5a081b50989c8612e4bcb16b51f59c0377272d4
JunH <tjdtls690@gmail.com>
2022-07-11 22:13:19 +0900

    :sparkles: feat : 여러 번 move 일 때 - 가 갯수만큼 제대로 찍히는지 확인

--- src/test/java/com/jun_hyeok/racing_car/domain/CarTest.java
+++ src/test/java/com/jun_hyeok/racing_car/domain/CarTest.java
@@ -17,4 +17,18 @@ public class CarTest {
         String currentSituation = car.getCurrentSituation();
         assertThat(currentSituation).isEqualTo("-");
     }
+
+    @Test
+    @DisplayName("여러번 움직인 상태일 때 표시")
+    void current_situation_multi(){
+        Car car = new Car("jun");
+        RacingCarGame racingCarGame = new RacingCarGame();
+        racingCarGame.stopIfNotMove(car, 4);
+        racingCarGame.stopIfNotMove(car, 3);
+        racingCarGame.stopIfNotMove(car, 8);
+        racingCarGame.stopIfNotMove(car, 2);
+        racingCarGame.stopIfNotMove(car, 9);
+        String currentSituation = car.getCurrentSituation();
+        assertThat(currentSituation).isEqualTo("---");
+    }
 }
