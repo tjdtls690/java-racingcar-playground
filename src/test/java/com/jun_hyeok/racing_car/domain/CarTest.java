51c62033da50ae0d4b4f84d33026e6fe785ea4b4
JunH <tjdtls690@gmail.com>
2022-07-11 22:17:33 +0900

    :art: refactor : 테스트 코드 리팩토링

--- src/test/java/com/jun_hyeok/racing_car/domain/CarTest.java
+++ src/test/java/com/jun_hyeok/racing_car/domain/CarTest.java
@@ -1,18 +1,24 @@
 package com.jun_hyeok.racing_car.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
+    private Car car;
+    private RacingCarGame racingCarGame;
 
+    @BeforeEach
+    void setUp() {
+        car = new Car("jun");
+        racingCarGame = new RacingCarGame();
+    }
 
     @Test
     @DisplayName("한 번 움직인 상태일 때 표시")
     void current_situation_one(){
-        Car car = new Car("jun");
-        RacingCarGame racingCarGame = new RacingCarGame();
         racingCarGame.stopIfNotMove(car, 4);
         String currentSituation = car.getCurrentSituation();
         assertThat(currentSituation).isEqualTo("-");
@@ -21,8 +27,6 @@ public class CarTest {
     @Test
     @DisplayName("여러번 움직인 상태일 때 표시")
     void current_situation_multi(){
-        Car car = new Car("jun");
-        RacingCarGame racingCarGame = new RacingCarGame();
         racingCarGame.stopIfNotMove(car, 4);
         racingCarGame.stopIfNotMove(car, 3);
         racingCarGame.stopIfNotMove(car, 8);
