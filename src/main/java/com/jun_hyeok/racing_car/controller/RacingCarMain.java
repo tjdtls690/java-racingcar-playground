7daa9d9e18e44de0b5a8893cce6d2710b28c8874
JunH <tjdtls690@gmail.com>
2022-07-21 11:41:35 +0900

    :sparkles: feat : 플레이 횟수 입력받기 기능 구현

--- src/main/java/com/jun_hyeok/racing_car/controller/RacingCarMain.java
+++ src/main/java/com/jun_hyeok/racing_car/controller/RacingCarMain.java
@@ -5,7 +5,10 @@ import com.jun_hyeok.racing_car.input.InputView;
 public class RacingCarMain {
     public static void main(String[] args) {
         InputView inputView = new InputView();
-        String carsName = inputView.getCarsName();
+        String carsName = inputView.getInput();
+        System.out.println(carsName);
         
+        int tryNum = inputView.getTryNum();
+        System.out.println(tryNum);
     }
 }
--- src/main/java/com/jun_hyeok/racing_car/input/InputView.java
+++ src/main/java/com/jun_hyeok/racing_car/input/InputView.java
@@ -6,18 +6,34 @@ import java.io.InputStreamReader;
 
 public class InputView {
     public static final String CARS_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+    public static final String TRY_NUM_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+    public static final String NUMBER_FORMAT_ERROR_MESSAGE = "숫자만 입력 가능합니다. 다시 입력해주세요.";
     private final BufferedReader br;
     
     public InputView() {
         br = new BufferedReader(new InputStreamReader(System.in));
     }
     
-    public String getCarsName() {
+    public String getInput() {
+        System.out.println(CARS_NAME_INPUT_MESSAGE);
+        return getString();
+    }
+    
+    private String getString() {
         try {
-            System.out.println(CARS_NAME_INPUT_MESSAGE);
             return br.readLine();
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+    
+    public int getTryNum() {
+        try {
+            System.out.println(TRY_NUM_INPUT_MESSAGE);
+            return Integer.parseInt(getString());
+        } catch (NumberFormatException e) {
+            System.out.println(NUMBER_FORMAT_ERROR_MESSAGE);
+            return getTryNum();
         }
     }
 }
