7e284705a8c0848d0be703cfab332b6bfbcdf8a8
JunH <tjdtls690@gmail.com>
2022-07-22 14:10:23 +0900

    :art: refactor : 에러 메시지 문자열 포장

--- src/main/java/com/jun_hyeok/racing_game/domain/Name.java
+++ src/main/java/com/jun_hyeok/racing_game/domain/Name.java
@@ -4,15 +4,17 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Name {
     public static final int MAX_CAR_NAME_LENGTH = 5;
+    public static final String CAR_NAME_NULL_OR_EMPTY_ERROR_MESSAGE = "차 이름은 null 또는 공백이 될 수 없습니다.";
+    public static final String CAR_NAME_LENGTH_ERROR_MESSAGE = "차 이름은 5자를 초과할 수 없습니다.";
     private final String name;
     
     public Name(String name) throws IllegalArgumentException {
         if (StringUtils.isBlank(name)) {
-            throw new IllegalArgumentException("차 이름은 null 또는 공백이 될 수 없습니다.");
+            throw new IllegalArgumentException(CAR_NAME_NULL_OR_EMPTY_ERROR_MESSAGE);
         }
         
         if (name.length() > MAX_CAR_NAME_LENGTH) {
-            throw new IllegalArgumentException("차 이름은 5자를 초과할 수 없습니다.");
+            throw new IllegalArgumentException(CAR_NAME_LENGTH_ERROR_MESSAGE);
         }
         
         this.name = name;
